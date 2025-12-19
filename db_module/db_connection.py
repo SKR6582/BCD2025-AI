409b2396f73aa6911aabc0f7cd7d8ed764c0edcb
SKR6582 <144501419+SKR6582@users.noreply.github.com>
2025-12-19 10:16:14 +0900

    TypeError Fixed from db_connection

--- db_module/db_connection.py
+++ db_module/db_connection.py
@@ -8,7 +8,7 @@ def get_connection():
     return pymysql.connect(
         host=os.getenv('DB_HOST'),          # 🔹 DB 주소
         user=os.getenv('DB_USER'),               # 🔹 DB 사용자명
-        port=os.getenv('DB_PORT'),
+        port=int(os.getenv('DB_PORT')),
         password=os.getenv('DB_PASSWORD'),         # 🔹 DB 비밀번호
         database=os.getenv('DB_NAME'),        # 🔹 DB 이름
         charset='utf8mb4',
